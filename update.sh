8edd417cc937bfd7c6cfc5e71c9d1b7684d31471
Iora Developer <max.fierke@iorahealth.com>
2018-01-29 22:01:02 -0600

    Update update.sh script to base off of qt@5.7 formula

--- update.sh
+++ update.sh
@@ -4,11 +4,11 @@ if [ ! hash brew 2>/dev/null ]; then
   echo "Please install brew: https://brew.sh"
 fi
 
-brew remove qt
-brew install --build-bottle qt --with-qtwebkit
-brew bottle qt
+brew remove qt qt@5.7
+brew install --build-bottle qt@5.7 --with-qtwebkit
+brew bottle qt@5.7
 
-brew cat qt > Formula/qt.rb
+brew cat qt@5.7 > Formula/qt@5.7.rb
 
 echo
 echo "#### IMPORTANT INFORMATION FOLLOWING! "
@@ -22,7 +22,7 @@ cat <<EOF
 Update the 'bottle' section in 'Formula/qt.rb' like so, but with the correct sha256sum (see above) and the correct version (again, see above)
 
   bottle do
-    root_url "https://github.com/ninech/homebrew-qt/releases/download/vX.X.X_1"
+    root_url "https://github.com/maxfierke/homebrew-qt/releases/download/vX.X.X_1"
     rebuild 1
     sha256 "2d54c0c0db8548e63134250950f9ac69d9ae52c101e397a1c1230d31bf3731ef" => :sierra
   end
@@ -31,5 +31,5 @@ Now commit the formula and push it to Github.
 Then create a new release [1] with the correct name (so that the URL above becomes valid!).
 Finally, upload the created tar.gz file to the release.
 
-[1] https://github.com/ninech/homebrew-qt/releases/new
+[1] https://github.com/maxfierke/homebrew-qt/releases/new
 EOF
